7ba0f732a2d6d363df3561723942d5dc9a47886a
zestycodes <118841160+zesty-codes@users.noreply.github.com>
2023-09-08 16:02:56 +0200

    Update AntiSlow.lua

--- AntiSlow.lua
+++ AntiSlow.lua
@@ -1,7 +1,7 @@
 local metatable = (getrawmetatable or get_raw_metatable or getraw_metatable or debug and debug.getmetatable)(game)
 local backup;
 backup = hookfunction(metatable.__newindex, newcclosure(function(self, property, value)
-    if property == "WalkSpeed" and self:IsA("Humanoid") and not checkcaller() then\
+    if property == "WalkSpeed" and self:IsA("Humanoid") and not checkcaller() then
         self[property] = 16
         return backup(self, property, 16)
     end
