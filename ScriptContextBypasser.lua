d5fa15d89f840eb4dfc847d6fe34840c42ce756a
zestycodes <118841160+zesty-codes@users.noreply.github.com>
2023-09-08 16:07:29 +0200

    Update ScriptContextBypasser.lua

--- ScriptContextBypasser.lua
+++ ScriptContextBypasser.lua
@@ -1,11 +1,11 @@
 local SC = game:GetService("ScriptContext")
 local getconnections = getconnections or get_connections or connections or dumpconnections
 function checkForBannedConnections()
-    local connections = getconnections(SC.Error)
-    for i, v in next, connections do
-        local method = v.Disconnect or v.Disable
-        v[method](v)
-        v[method](v[method])
+    for i,v in pairs(getconnections(SC.Error)) do
+        if i and v then
+            v:Disconnect()
+            v = nil
+        end
     end
 end
 local metatable = (getrawmetatable or get_raw_metatable or getraw_metatable or debug and debug.getmetatable)(game)
