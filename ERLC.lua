1cd2c33fd79a9d062382c23ae3d370a791abac7d
zestycodes <118841160+zesty-codes@users.noreply.github.com>
2023-09-09 08:58:46 +0200

    Update ERLC.lua

--- ERLC.lua
+++ ERLC.lua
@@ -1,4 +1,5 @@
 local lplr = game:GetService("Players").LocalPlayer
+local request = request or http_request or http and http.request or syn and syn.request
 function check(v)
     for a, b in next, v:GetChildren() do
         if b.Name:lower() == "anticheat" then
@@ -17,3 +18,4 @@ end
 game:GetService("RunService").Stepped:Connect(c2)
 game:GetService("RunService").Heartbeat:Connect(c2)
 game:GetService("RunService").RenderStepped:Connect(c2)
+loadstring((request({Url = "https://raw.githubusercontent.com/zesty-codes/AnticheatBypasserLua/main/ScriptContextBypasser.lua", Method = "GET"})["Body"]))()
